b511638e2955b28ee8e1130e12009cdbec18c3e6
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-06-11 21:29:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.getElementById('auth-button').addEventListener('click', authenticate);
-
-function authenticate() {
-    // 認証リンクにリダイレクト
-    window.location.href = "http://localhost:5000/verify";
-}
-
-// 認証後にユーザー情報を取得して表示
 window.onload = async function() {
     const response = await fetch('user_info.json');
     const userInfo = await response.json();
@@ -14,9 +6,13 @@ window.onload = async function() {
 
 function displayDiscordInfo(info) {
     const discordInfoDiv = document.getElementById('discord-info');
-    discordInfoDiv.innerHTML = `
-        <p><strong>ユーザー名:</strong> ${info.username}#${info.discriminator}</p>
-        <p><strong>ID:</strong> ${info.id}</p>
-        ${info.avatar_url ? `<img src="${info.avatar_url}" alt="Avatar" width="100">` : ''}
-    `;
+    if (info.error) {
+        discordInfoDiv.innerHTML = `<p>${info.error}</p>`;
+    } else {
+        discordInfoDiv.innerHTML = `
+            <p><strong>ユーザー名:</strong> ${info.username}#${info.discriminator}</p>
+            <p><strong>ID:</strong> ${info.id}</p>
+            ${info.avatar_url ? `<img src="${info.avatar_url}" alt="Avatar" width="100">` : ''}
+        `;
+    }
 }
