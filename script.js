9dd69d86e14b27aa472e221fbc31d83674d639cf
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-06-12 15:34:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const response = await fetch('/user_info.json');
-        const data = await response.json();
+    const params = new URLSearchParams(window.location.search);
+    const username = params.get('username');
+    const discriminator = params.get('discriminator');
+    const avatar_url = params.get('avatar_url');
 
-        const userInfoDiv = document.getElementById('user-info');
-        if (data.error) {
-            userInfoDiv.innerHTML = '<p>ユーザー情報を取得できませんでした。再度認証を行ってください</p>';
-        } else {
-            userInfoDiv.innerHTML = `
-                <p>ユーザー名: ${data.username}#${data.discriminator}</p>
-                <img src="${data.avatar_url}" alt="Avatar" width="80" height="80">
-            `;
-        }
-    } catch (error) {
-        console.error('Error fetching user info:', error);
+    const userInfoDiv = document.getElementById('user-info');
+    if (username && discriminator) {
+        userInfoDiv.innerHTML = `
+            <p>ユーザー名: ${username}#${discriminator}</p>
+            <img src="${avatar_url}" alt="Avatar" width="80" height="80">
+        `;
+    } else {
+        userInfoDiv.innerHTML = '<p>ユーザー情報を取得できませんでした。再度認証を行ってください</p>';
     }
 });
