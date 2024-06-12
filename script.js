1a5fee9c356d9bcfc68b08b95bf0b5a283667224
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-06-12 18:04:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var loginButton = document.getElementById('login-button');
     if (loginButton) {
         loginButton.addEventListener('click', function() {
-            window.location.href = 'http://localhost:5000/login';
+            // Discordの認証ページへリダイレクト
+            window.location.href = 'https://republicofauech.github.io/vearithy/';
         });
     } else {
         console.error('Login button element not found');
@@ -10,13 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     fetch('http://localhost:5000/user_info.json', {
         method: 'GET',
-        credentials: 'include'  // Include credentials in the request
+        credentials: 'include'  // 認証情報を含める
     })
     .then(response => response.json())
     .then(data => {
         if (!data.error) {
             document.getElementById('user-info').style.display = 'block';
-            document.getElementById('avatar').src = data.avatar_url;
+            document.getElementById('avatar').src = ''; // ユーザーのアバター画像のURLを設定する
             document.getElementById('username').innerText = `${data.username}#${data.discriminator}`;
         } else {
             console.error('Error fetching user info:', data.error);
