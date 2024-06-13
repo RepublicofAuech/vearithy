4c4f5f76d771966714aa3c834402519c80891c98
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-06-13 17:12:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/; samesite=None; secure";
+        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
     }
 
     function getQueryParam(param) {
