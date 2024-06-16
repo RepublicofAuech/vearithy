771a456040b342bca7b92bd506e99d42287ced65
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-06-17 08:35:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const authButton = document.getElementById('auth-button');
-    const accessToken = getQueryParam('access_token') || getCookie('access_token');
+    const accessToken = getQueryParam('access_token');
 
     if (accessToken) {
         // アクセストークンがある場合、ユーザー情報を取得する処理
@@ -47,9 +47,3 @@ document.addEventListener('DOMContentLoaded', function() {
         authButton.style.display = 'inline-block';
     }
 });
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
